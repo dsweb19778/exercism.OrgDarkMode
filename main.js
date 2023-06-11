fb0123ac689ba70691e9c1cd70d8aff8d3a4fedb
Nabil <assadouni@gmail.com>
2023-06-11 21:34:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-getbody = document.getElementsByTagName('body')
-getbody[0].className = 'namespace- controller-premium action-show theme-dark user-signed_in'
+let url = document.baseURI
+let counter = 0
+
+darkmoding()
+
+function darkmoding() {
+  getbody = document.getElementsByTagName('body')
+  getbody[0].className = 'namespace- controller-premium action-show theme-dark user-signed_in'
+}
+
+function detecting_url_changes() {
+  counter += url.length
+  if (counter > url.length || counter < url.length) {
+    darkmoding()
+  } else if (counter == url.length) {
+    console.log('url not changed')
+  }
+}
+
+setInterval(detecting_url_changes, 2000)
