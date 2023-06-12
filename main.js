6e3223df02108432d865ad1df9f6dd3fff10c94a
Nabil <assadouni@gmail.com>
2023-06-12 12:14:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 let url = document.baseURI
+let getbody = document.getElementsByTagName('body')
 let counter = 0
 
 // enable dark mode
 darkmoding()
 
+
 function darkmoding() {
-  if (url.match('edit')[0] == 'edit') {
-    getbody = document.getElementsByTagName('body')
-    getbody[0].className = 'namespace-tracks controller-exercises action-edit theme-dark user-signed_in'
-  } else {
-    getbody = document.getElementsByTagName('body')
+
+  try {
+    if (url.match('edit')[0] == 'edit') {
+      getbody[0].className = 'namespace-tracks controller-exercises action-edit theme-dark user-signed_in'
+    }
+  } catch {
     getbody[0].className = 'namespace- controller-premium action-show theme-dark user-signed_in'
   }
 
@@ -20,9 +23,8 @@ function detecting_url_changes() {
   if (counter > url.length || counter < url.length) {
     darkmoding()
   } else if (counter == url.length) {
-    console.log('not changed')
+    console.log('url not changed')
   }
 }
 
 setInterval(detecting_url_changes, 4000)
-
